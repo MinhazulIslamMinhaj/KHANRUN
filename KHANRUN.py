d8d15b94d3fb283fd66c83eef1ba16de8560ac5d
MinhazulIslamMinhaj <143911340+MinhazulIslamMinhaj@users.noreply.github.com>
2023-09-06 04:39:55 +0600

    Add files via upload

--- KHANRUN.py
+++ KHANRUN.py
@@ -367,7 +367,7 @@ def api(self, uid, pwx):
                          pw = pw.lower() 
                          ses = requests.Session() 
                          headers = {
-U+00A0                         
+ U+00A0                         
     'authority': 'mbasic.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9',
