941dae430803621dc4aa867e817d3db3db72dfab
MinhazulIslamMinhaj <143911340+MinhazulIslamMinhaj@users.noreply.github.com>
2023-09-06 04:42:24 +0600

    Add files via upload

--- KHANRUN.py
+++ KHANRUN.py
@@ -367,26 +367,6 @@ def api(self, uid, pwx):
                          pw = pw.lower() 
                          ses = requests.Session() 
                          headers = {
- U+00A0                         
-    'authority': 'mbasic.facebook.com',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    'dpr': '2',
-    'sec-ch-prefers-color-scheme': 'light',
-    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
-    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-model': '"Infinix X665B"',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-ch-ua-platform-version': '"11.0.0"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
-    'viewport-width': '980',}
                                  "x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)),  
                                  "x-fb-sim-hni": str(random.randint(20000, 40000)),  
                                  "x-fb-net-hni": str(random.randint(20000, 40000)),  
