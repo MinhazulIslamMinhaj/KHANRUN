c216f6e66af37e06de394bbbf51d1e96fc219646
MinhazulIslamMinhaj <143911340+MinhazulIslamMinhaj@users.noreply.github.com>
2023-09-06 04:18:44 +0600

    Add files via upload

--- KHANRUN.py
+++ KHANRUN.py
@@ -93,17 +93,17 @@ def notice():
   
   
   
- plist = (platform.uname())[2] 
- basex = plist 
- basex1 = basex.encode('ascii') 
- basex2 = base64.b64encode(basex1) 
- basex3 = basex2.decode('ascii') 
- base4 = (basex3).upper() 
- basesplit = base4.replace('=', 'X').replace('A', '3').replace('B', '9').replace('C', '7').replace('D', '1').replace('E', '4').replace('M', '2').replace('L', '6').replace('F', '8').replace('N', 'E').replace('T', '8') 
+plist = (platform.uname())[2] 
+basex = plist 
+basex1 = basex.encode('ascii') 
+basex2 = base64.b64encode(basex1) 
+basex3 = basex2.decode('ascii') 
+base4 = (basex3).upper() 
+basesplit = base4.replace('=', 'X').replace('A', '3').replace('B', '9').replace('C', '7').replace('D', '1').replace('E', '4').replace('M', '2').replace('L', '6').replace('F', '8').replace('N', 'E').replace('T', '8') 
   
   
- class Main: 
-         def __init__(self): 
+class Main: 
+        def __init__(self): 
                  self.id = [] 
                  self.ok = [] 
                  self.cp = [] 
