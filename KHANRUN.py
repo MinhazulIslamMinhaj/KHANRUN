57e117c9fd958d3ef7ade3d60c07e26f72c06321
MinhazulIslamMinhaj <143911340+MinhazulIslamMinhaj@users.noreply.github.com>
2023-09-06 06:52:58 +0600

    Add files via upload

--- KHANRUN.py
+++ KHANRUN.py
@@ -137,10 +137,10 @@ class Main:
  \t\033[1;97m[\x1b[1;97m\x1b[1;41m Facebook : Minhaj Khan] 
  ───────────────────────────────────────────────────────×  
      """) 
-                 print("%s [%s•%s] %sTOOL NAME : %sFB ID CLONING"%
-                 print("%s [%s•%s] %sVERSION   : 
-                 print("%s [%s•%s] %sYOUR KEY  : "%s
-                 print("%s [%s•%s] %sSTATUS    : "%s
+                 print("%s [(U+2022)] %sTOOL NAME : %sFB ID CLONING"%
+                 print(%s [(U+2022)] %sVERSION  : 
+                 print("%s [(U+2022)] %sYOUR KEY  : "%s
+                 print("%s [(U+2022)] %sSTATUS    : "%s
                  print("───────────────────────────────────────────────────────× ") 
                  print(" \t\033[1;97m[\x1b[1;97m\x1b[1;41m Mr Khan\x1b[0m\x1b[1;97m] \033[1;37m ") 
                  subprocess.check_output(["am", "start", "https://chat.whatsapp.com/ICLU1npbLZR8x61KyX5fUR"]) 
